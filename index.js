a16dead0427611307d3c590194bd373700f4d320
Will-MiniPolis <39627424+Will-MiniPolis@users.noreply.github.com>
2018-05-25 18:24:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -14,18 +14,38 @@ bot.on("message", async message => {
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
-
-  if (cmd === `!comandos`){
-    message.channel.send("```md\n# Comandos do BOT: \n\n• !jogar (Utilize este comando para jogar MiniPólis) \n• !info (Informações sobre o BOT) \n• Comando 3 (Em breve) \n```");
+  
+if (cmd === `!teste`){
+  message.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "This is an embed",
+    url: "http://google.com",
+    description: "This is a test embed to showcase what they look like and what they can do.",
+    fields: [{
+        name: "Fields",
+        value: "They can have different fields with small headlines."
+      },
+      {
+        name: "Masked links",
+        value: "You can put [masked links](http://google.com) inside of rich embeds."
+      },
+      {
+        name: "Markdown",
+        value: "You can put all the *usual* **__Markdown__** inside of them."
+      }
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© Example"
+	}
   }
-
-  if (cmd === `!jogar`){
-    message.channel.send(":video_game: Jogar MiniPólis :video_game: \n\nPara testar a versão ALPHA do MiniPólis basta acessar nosso site: \n:heart:    https://ALPHA.MiniPolis.com.br/   :heart:");
-  }
-
-  if (cmd === `!info`){
-    message.channel.send("```md\n# BOT - MiniPólis: \n\n• Desenvolvedor: Will Ajudante-MP \n• Versão: 1.0.0 \n• Atualização: 25/05/2018 \n• Nome do BOT: Moderador \n• Idioma: Português (Brasil) \n• Proprietário: MiniPolis.com.br \n```");
-  }
+})
+  
 });
 
 bot.login(process.env.TOKEN);
