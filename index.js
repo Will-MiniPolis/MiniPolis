65447048afbd02b18fa50863394b8177b1c2590b
Will-MiniPolis <39627424+Will-MiniPolis@users.noreply.github.com>
2018-05-25 18:05:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ bot.on("message", async message => {
   let args = messageArray.slice(1);
 
   if (cmd === `!comandos`){
-    message.channel.send("```md\n# Comandos do BOT: \n\n• !jogar (Utilize este comando para jogar MiniPólis) \n• Comando 2 (Em breve) \n• Comando 3 (Em breve) \n```");
+    message.channel.send("```md\n# Comandos do BOT: \n\n• !jogar (Utilize este comando para jogar MiniPólis) \n• !info (Informações sobre o BOT) (Em breve) \n• Comando 3 (Em breve) \n```");
   }
 
   if (cmd === `!jogar`){
