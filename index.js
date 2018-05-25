124403c5cf8c763313ddf4a34312f9b023ff8ade
Will-MiniPolis <39627424+Will-MiniPolis@users.noreply.github.com>
2018-05-25 18:59:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ if (cmd === `!info`){
     title: "Informações do BOT: \n",
     fields: [{
         name: "Desenvolvedor:          Versão:",
-        value: "Will Ajudante-MP"       1.0.0"
+        value: "Will Ajudante-MP        1.0.0"
       },
       {
         name: "  !Jogar  ",
