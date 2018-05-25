564311f358491afd5fd0f0a41ce3096ce49b0b1e
Will-MiniPolis <39627424+Will-MiniPolis@users.noreply.github.com>
2018-05-25 18:44:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
   console.log(`-----||-----||-----||----- \n BOT ATIVADO COM SUCESSO! \n-----||-----||-----||-----`);
-  bot.user.setActivity(`MiniPólis  -  !Comandos`);
+  bot.user.setActivity(`MiniPólis  -  !Comandos`);
 });
 
 bot.on("message", async message => {
+	
 
   if (message.author.bot) return;
   if (message.channel.type === "dm") return;
@@ -14,33 +15,33 @@ bot.on("message", async message => {
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
+  
 
-if (cmd === `!teste`){
+if (cmd === `!comandos`){
   message.channel.send({embed: {
     color: 3447003,
     author: {
       name: bot.user.username,
       icon_url: bot.user.avatarURL
     },
-    title: "This is an embed",
-    url: "http://google.com",
-    description: "This is a test embed to showcase what they look like and what they can do.",
+    title: "Comandos do BOT: \n",
     fields: [{
-        name: "Fields",
-        value: "They can have different fields with small headlines."
+        name: "!Info",
+        value: "Informações sobre o BOT. \n"
       },
       {
-        name: "Masked links",
-        value: "You can put [masked links](http://google.com) inside of rich embeds."
+        name: "!Jogar",
+        value: "Utilize este comando para jogar MiniPólis. \n"
       },
       {
         name: "Markdown",
         value: "You can put all the *usual* **__Markdown__** inside of them."
       }
     ],
-	}}
-	)}
-	  
+  }})
+}
+
+
 });
 
 bot.login(process.env.TOKEN);
