6890670fbeb194f3cc8e3e3c42b14d640b0f15ae
Will-MiniPolis <39627424+Will-MiniPolis@users.noreply.github.com>
2018-05-26 10:42:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,141 +1,71 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client({
-    disableEveryone: true
-});
+// Calling Packages
+const Discord = require('discord.js');
+const bot = new Discord.Client();
 
-var Info = new Discord.RichEmbed()
-    .setAuthor(" MOD - [BR] ", "https://i.imgur.com/DjkjbxW.png")
-    .setTitle(" ►    Informações sobre o BOT    ◄ ")
-    .setColor(0xff1515)
-    .setDescription(" \u200b ")
-    .setFooter(" © MiniPólis - Todos os direitos reservados. ", "https://i.imgur.com/flUGdY9.png")
-    .setURL("https://facebook.com/Will.MiniPolis")
-    .addField(" • Desenvolvedor: Will Ajudante-MP ", " • Versão: 1.0.0 ")
-    .addField(" • Atualização: 26/05/2018 ", " • Nome do BOT: MOD - [BR] ")
-    .addField(" • Idioma: Português (Brasil) ", " • Proprietário: [MiniPólis](https://www.minipolis.com.br/) ");
+// Global Settings
+const prefix = '~'; // This is the prefix, you can change it to whatever you want.
 
-var Jogar = new Discord.RichEmbed()
-    .setAuthor(" MOD - [BR] ", "https://i.imgur.com/DjkjbxW.png")
-    .setTitle(" :video_game:   Jogar MiniPólis   :video_game: ")
-    .setColor(0x00ff40)
-    .setDescription(" \u200b ")
-    .setFooter(" © MiniPólis - Todos os direitos reservados. ", "https://i.imgur.com/flUGdY9.png")
-    .setURL("https://minipolis.com.br")
-    .addField("   Para jogar a versão ALPHA do MiniPólis acesse:   ", "     [alpha.minipolis.com.br](https://alpha.minipolis.com.br/)     ");
+// Listener Event: Runs whenever a message is received.
+bot.on('message', message => {
 
-var Ajudantes = new Discord.RichEmbed()
-    .setAuthor(" MOD - [BR] ", "https://i.imgur.com/DjkjbxW.png")
-    .setTitle(" (+)   Ajudantes do MiniPólis   (+) ")
-    .setColor(0xff1515)
-    .setDescription(" \u200b ")
-    .setFooter(" © MiniPólis - Todos os direitos reservados. ", "https://i.imgur.com/flUGdY9.png")
-    .setThumbnail("https://i.imgur.com/JzrVRgv.png")
-    .setURL("https://minipolis.com.br/")
-    .addField(" • Ale Ajudante-MP ", " • Gelff Ajudante-MP ")
-    .addField(" • Will Ajudante-MP ", " • William Ajudante-MP ");
+    // Variables - Variables make it easy to call things, since it requires less typing.
+    let msg = message.content.toUpperCase(); // This variable takes the message, and turns it all into uppercase so it isn't case sensitive.
+    let sender = message.author; // This variable takes the message, and finds who the author is.
+    let cont = message.content.slice(prefix.length).split(" "); // This variable slices off the prefix, then puts the rest in an array based off the spaces
+    let args = cont.slice(1); // This slices off the command in cont, only leaving the arguments.
 
-var Equipe = new Discord.RichEmbed()
-    .setAuthor(" MOD - [BR] ", "https://i.imgur.com/DjkjbxW.png")
-    .setTitle(" (E)   Equipe do MiniPólis   (E) ")
-    .setColor(0xff1515)
-    .setDescription(" \u200b ")
-    .setFooter(" © MiniPólis - Todos os direitos reservados. ", "https://i.imgur.com/flUGdY9.png")
-    .setThumbnail("https://i.imgur.com/T1KZNJ6.png")
-    .setURL("https://minipolis.com.br/")
-    .addField(" • Andrew Equipe-MP ", " • Victor Equipe-MP ");
+    // Commands
 
-var Moderadores = new Discord.RichEmbed()
-    .setAuthor(" MOD - [BR] ", "https://i.imgur.com/DjkjbxW.png")
-    .setTitle(" (☆)   Moderadores do MiniPólis   (☆) ")
-    .setColor(0xff1515)
-    .setDescription(" \u200b ")
-    .setFooter(" © MiniPólis - Todos os direitos reservados. ", "https://i.imgur.com/flUGdY9.png")
-    .setThumbnail("https://i.imgur.com/DjkjbxW.png")
-    .setURL("https://minipolis.com.br/")
-    .addField(" • Nenhum integrante no momento. ", "\u200b");
+    // Ping
+    if (msg === prefix + 'PING') { // This checks if msg (the message but in all caps), is the same as the prefix + the command in all caps.
 
-var Administradores = new Discord.RichEmbed()
-    .setAuthor(" MOD - [BR] ", "https://i.imgur.com/DjkjbxW.png")
-    .setTitle(" (A)   Administradores do MiniPólis   (A) ")
-    .setColor(0xff1515)
-    .setDescription(" \u200b ")
-    .setFooter(" © MiniPólis - Todos os direitos reservados .", "https://i.imgur.com/flUGdY9.png")
-    .setThumbnail("https://i.imgur.com/XpxUHn4.png")
-    .setURL("https://minipolis.com.br/")
-    .addField(" • Rky Smart ", "• Tiago Admin-MP ");
+        // Now, let's send a response.
+        message.channel.send('Ping!'); // This 'sends' the message to the channel the message was in. You can change what is in the message to whatever you want.
 
-bot.on("ready", async () => {
-    console.log(`-----||-----||-----||----- \n BOT ATIVADO COM SUCESSO! \n -----||-----||-----||-----`);
-    bot.user.setActivity(`Utilize: /comandos`);
-});
-
-bot.on("message", async message => {
-
-    if (message.author.bot) return;
-    if (message.channel.type === "dm") return;
-
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0];
-    let args = messageArray.slice(1);
-
-    if (cmd === `/info`) {
-        message.channel.send(Info);
     }
 
-    if (cmd === `/jogar`) {
-        message.channel.send(Jogar);
-    }
 
-    if (cmd === `/ajudantes`) {
-        message.channel.send(Ajudantes);
-    }
-
-    if (cmd === `/equipe`) {
-        message.channel.send(Equipe);
-    }
-
-    if (cmd === `/moderadores`) {
-        message.channel.send(Moderadores);
-    }
-
-    if (cmd === `/administradores`) {
-        message.channel.send(Administradores);
-    }
-
-    if (cmd === `/comandos`) {
-        message.channel.send("```diff\n- Este comando não está disponível no momento. \n```");
-    }
-    
-    
-    
-    
-    let msg = message.content.toUpperCase();
-    let sender = message.author;
-    let cont = message.content.slice().split(" ");
-    let teste = cont.slice(1);
-    
-if (msg.startsWith('/apagar')) {
+    // Purge
+    if (msg.startsWith(prefix + 'PURGE')) { // This time we have to use startsWith, since we will be adding a number to the end of the command.
+        // We have to wrap this in an async since awaits only work in them.
         async function purge() {
-            message.delete();
-            if (!message.member.roles.find("name", "bot-commander")) {
-                message.channel.send('You need the \`bot-commander\` role to use this command.');
+            message.delete(); // Let's delete the command message, so it doesn't interfere with the messages we are going to delete.
+
+            // Now, we want to check if the user has the `bot-commander` role, you can change this to whatever you want.
+            if (!message.member.roles.find("name", "bot-commander")) { // This checks to see if they DONT have it, the "!" inverts the true/false
+                message.channel.send('You need the \`bot-commander\` role to use this command.'); // This tells the user in chat that they need the role.
+                return; // this returns the code, so the rest doesn't run.
+            }
+
+            // We want to check if the argument is a number
+            if (isNaN(args[0])) {
+                // Sends a message to the channel.
+                message.channel.send('Please use a number as your arguments. \n Usage: ' + prefix + 'purge <amount>'); //\n means new line.
+                // Cancels out of the script, so the rest doesn't run.
                 return;
             }
-            if (isNaN(teste[0])) {
-                message.channel.send('Please use a number as your arguments. \n Usage: purge <amount>');
-                return;
-            }
-            const fetched = await message.channel.fetchMessages({limit: teste[0]});
-            console.log(fetched.size + ' messages found, deleting...');
+
+            const fetched = await message.channel.fetchMessages({limit: args[0]}); // This grabs the last number(args) of messages in the channel.
+            console.log(fetched.size + ' messages found, deleting...'); // Lets post into console how many messages we are deleting
+
+            // Deleting the messages
             message.channel.bulkDelete(fetched)
-                .catch(error => message.channel.send(`Error: ${error}`));
+                .catch(error => message.channel.send(`Error: ${error}`)); // If it finds an error, it posts it into the channel.
+
         }
-        purge();
+
+        // We want to make sure we call the function whenever the purge command is run.
+        purge(); // Make sure this is inside the if(msg.startsWith)
+
     }
-    
-    
-    
-    
 });
 
-bot.login(process.env.TOKEN);
+// Listener Event: Runs whenever the bot sends a ready event (when it first starts for example)
+bot.on('ready', () => {
+
+    // We can post into the console that the bot launched.
+    console.log('Bot started.');
+
+});
+
+bot.login('TOKEN');
