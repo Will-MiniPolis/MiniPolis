477276b4fac783edd6b5d268c2d06cb00ae2f545
Will-MiniPolis <39627424+Will-MiniPolis@users.noreply.github.com>
2018-05-26 12:12:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ bot.on("message", async message => {
     
     
 if (cmd === prefix + 'ping') {
-        message.channel.send(sender + " seu ping está " + new Date().getTime() - message.createdTimestamp + " ms.");
+        message.channel.send(message.author + ' seu ping está ' + new Date().getTime() - message.createdTimestamp + ' ms.');
     }
     
     
