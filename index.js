dc82a350796ce1a308a2bb73bf5891b079630258
Will-MiniPolis <39627424+Will-MiniPolis@users.noreply.github.com>
2018-05-25 22:03:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ if (cmd === `!jogar`){message.channel.send({embed: {
       icon_url: bot.user.avatarURL
     },
     title: ":video_game:   Jogar MiniPólis   :video_game:",
-    description: "|-|-|-|-|-|-|-|-|-|-|",
+    description: "\a",
     fields: [{
         name: "Para jogar a versão ALPHA do MiniPólis acesse:   ",
         value: "[ALPHA.MiniPolis.com.br](https://Alpha.MiniPolis.com.br/)"
