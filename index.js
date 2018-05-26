dd7e2474e2751813c911edb443887c1185032294
Will-MiniPolis <39627424+Will-MiniPolis@users.noreply.github.com>
2018-05-26 12:03:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ bot.on("message", async message => {
     
     if (cmd === prefix + 'ping') {
     const m = await message.channel.send("Ping?");
-    m.edit('Seu ping é ${m.createdTimestamp - message.createdTimestamp}ms.');
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
   }
     
     
