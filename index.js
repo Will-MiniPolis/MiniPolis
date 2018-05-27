c13102d527175116f089afd0d8b66bb74bb5adc7
Will-MiniPolis <39627424+Will-MiniPolis@users.noreply.github.com>
2018-05-27 18:17:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,19 +4,12 @@ const prefix = '/';
 
 bot.on("ready", async () => {
     console.log(`-----||-----||-----||----- \n BOT ATIVADO COM SUCESSO! \n -----||-----||-----||-----`);
-    bot.user.setActivity('Utilize: /comandos', 'https://www.twitch.tv/');
+    bot.user.setActivity('Utilize: /comandos');
     bot.user.setStatus('dnd');
 });
 
 bot.on('guildMemberAdd', member => {
     member.guild.channels.get('450337831684931624').send(member.user + ',\n```md\n# Seja bem-vindo(a) ao Discord do MiniPólis!\n# Welcome to the MiniPólis Discord!\n``````fix\n- Utilize o comando: | Use the command:\n• /pt (Caso seu idioma seja Português)\n• /en (If your language is English)\n```'); 
-});
-
-bot.on("guildMemberAdd", function(member) {
-    if (cmd === prefix + 'pt') {
-        let role = member.guild.roles.find("name", "Português");
-        member.addRole(role).catch(console.error);
-    }
 });
 
 var Comandos = new Discord.RichEmbed()
