0c05769e972c1b1762451a750c1a3dcbbfe05f8e
Will-MiniPolis <39627424+Will-MiniPolis@users.noreply.github.com>
2018-05-27 13:51:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -178,20 +178,10 @@ bot.on("message", async message => {
     
     
     
-    
-    const newUsers = [];
-    
-    bot.on("guildMemberAdd", (member) => {
-        const guild = member.guild;
-        if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
-        newUsers[guild.id].set(member.id, member.user);
 
-    if (newUsers[guild.id].size > 10) {
-        const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
-        guild.channels.get(guild.id).send("Seja bem vindo!\n" + userlist);
-        newUsers[guild.id].clear();
-  }
-});
+    bot.on('guildMemberAdd', member => {
+        member.guild.channels.get('450337831684931624').send('**' + member.user.username + '**, entrou no Discord do MiniPólis!'); 
+    });
     
     
     
